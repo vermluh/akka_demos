a6b60f07ac7c7cb924bdb721eab2492fa4d9c934
Florian Simon <github@verm-luh.de>
2016-09-16 16:16:07 +0200

    first

---
+++ AkkaFSM/src/AkkaFSM/MyFSM.cs
@@ -0,0 +1,49 @@
+﻿namespace AkkaFSM
+{
+    using System;
+    using Akka.Actor;
+
+    public class MyFSM : FSM<int, object>
+    {
+        #region constructor
+        public MyFSM(IActorRef target)
+        {
+            this.Target = target;
+            this.StartWith(0, new object());
+
+            When(0, evnt =>
+            {
+                if (evnt.FsmEvent.Equals("tick")) return this.GoTo(1);
+                return null;
+            });
+
+            When(1, evnt =>
+            {
+                if (evnt.FsmEvent.Equals("tick")) return this.GoTo(0);
+                return null;
+            });
+
+            WhenUnhandled(evnt =>
+            {
+                if (evnt.FsmEvent.Equals("reply")) return this.Stay().Replying("reply");
+                return null;
+            });
+
+            this.Initialize();
+        }
+        #endregion
+
+        #region properties
+        public IActorRef Target { get; private set; }
+        #endregion
+
+        #region methods
+        #region protected override void PreRestart(Exception, object)
+        protected override void PreRestart(Exception reason, object message)
+        {
+            Target.Tell("restarted");
+        }
+        #endregion
+        #endregion
+    }
+}
